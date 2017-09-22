2feee9ab433656f5733363c085eeaae6457aa2c2
Marcelo Sales <masales@redhat.com>
2017-09-22 11:55:20 -0300

    update index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>A Simple PHP File</title>
 </head>
 <body>
-    <h1><?php echo "Hello, Red Hat Developers World from PHP" . PHP_VERSION . "\n"; ?></h1>
+    <h1><?php echo "Hello, Red Hat Developers World from PHP - " . PHP_VERSION . "\n"; ?></h1>
     <h2><?php echo gethostname(); ?></h2>
 </body>
 </html>
